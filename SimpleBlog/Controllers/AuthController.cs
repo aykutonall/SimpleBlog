b38a89e42821be587be429669cbbf1af458fe140
DESKTOP-TSGA8O3\aykut <aykutonall@gmail.com>
2017-03-18 17:23:27 +0300

    First Commit

---
+++ SimpleBlog/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using SimpleBlog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace SimpleBlog.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View(new AuthLogin());
+        }
+
+        [HttpPost]
+        public ActionResult Login(AuthLogin form)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            if (form.Username.Length < 5)
+            {
+                ModelState.AddModelError("Username", "Username must be 5 characters at least");
+                return View(form);
+            }
+
+            FormsAuthentication.SetAuthCookie(form.Username, true);
+
+            return Content("The form is valid.");
+            
+        }
+    }
+}
